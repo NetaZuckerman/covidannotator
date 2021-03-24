5ac7745e43ad4c79f471dcd84adfd8c7b4bd8565
netanelxa <58979197+netanelxa@users.noreply.github.com>
2021-03-24 16:45:15 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -7,21 +7,46 @@ from io import StringIO
 import numpy as np
 
 
+def getRegion(i,regionsList):
+    regionTitles = []
+    for region in regionsList:
+        if i + 1 in range(int(region["start"]), int(region["end"])):
+            regionTitles.append(region["id"])
+    if len(regionTitles) > 1:
+        return str(regionTitles[len(regionTitles)-1])
+    else: return str(regionTitles[0])
+
 def main():
-    with open('names.csv', 'w', newline='') as csvfile:
-        fieldnames = ['Sequence ID', 'Reference Nucleotide','Mutation nucleotide','location']
+    # importing the Regions list
+    with open('regions.csv', 'r') as f:
+        reader = csv.reader(f)
+        my_list = []
+        for row in reader:
+            my_list.append({'segment': row[0], 'id': row[1],
+                            'region': row[2], 'start': row[3], 'end': row[4], 'function': row[5]})
+        regionsList = my_list[1:]
+
+    # Creating the output csv file
+    with open('output.csv', 'w', newline='') as csvfile:
+        fieldnames = ['Sequence ID', 'Reference Nucleotide', 'Mutation nucleotide', 'location', 'nuc name','protein']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
-        records = list(SeqIO.parse("names.fasta", "fasta"))
-        a=records[0].id
+        # parsing the pasta file
+        records = list(SeqIO.parse("FrenchVar_aligned.fasta", "fasta"))
+        # the first sequence is the reference
         referenceSequence = list(records[0])
         otherSequences = records[1:]
+        # Find mutations = iterating over each sequence and compare to the reference sequence
         for record in otherSequences:
-            a=record.id
-            a=list(record)
-            for i,nucleotide in enumerate((record)):
+            a = record.id
+            a = list(record)
+            for i, nucleotide in enumerate((record)):
                 if nucleotide != "-" and referenceSequence[i] != nucleotide:
-                    writer.writerow({'Sequence ID': record.id, 'Reference Nucleotide': referenceSequence[i],'Mutation nucleotide':nucleotide,'location':i})
+                    regionTitle=getRegion(i,regionsList)
+                    # Each mutation is written to the output csv file
+                    writer.writerow({'Sequence ID': record.id, 'Reference Nucleotide': referenceSequence[i],
+                                     'Mutation nucleotide': nucleotide, 'location': i + 1,
+                                     'nuc name': str(i + 1) + " " + referenceSequence[i] + " -> " + nucleotide,'protein':str(regionTitle)})
 
 
 if __name__ == "__main__":
