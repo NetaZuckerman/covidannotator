dd2935a396064d359d14862438b067b59619bfe0
netanelxa <58979197+netanelxa@users.noreply.github.com>
2021-05-27 12:50:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import sys
 
 def main():
     all_mutations = pd.read_csv("all_mutations.csv")
+    b117muts = pd.read_csv("b117muts.csv")
     uniqueIDs=pd.unique(all_mutations["Sequence ID"])
     uniqueMuts=pd.unique(all_mutations["nuc name"])
     numofSequences=len(uniqueIDs)
@@ -25,11 +26,22 @@ def main():
     freqTable['Freq'] = freqTable['nuc name'].map(freqs)
     freqTable.sort_values(by=['protein', 'Freq'],ascending=False,inplace=True)
     freqTable=freqTable.loc[freqTable['Freq'] >= 2]
+    mapping = dict(b117muts[['nucleotide', 'lineage original']].values)
+    freqTable['isUKLineage'] = freqTable['nuc name'].map(mapping)
+    num_muts = len(freqTable.index) - 1
+    if num_muts <= 0:
+        num_muts = 0
+    geneFreq = freqTable['protein'].value_counts()  # + " \ " + str(num_muts)
+    geneFreq['total'] = num_muts
+    geneFreq = pd.DataFrame(geneFreq)
+    if not geneFreq.empty:
+        geneFreq['Freq'] = geneFreq / num_muts * 100
 
-    freqTable.to_csv("Freq_Table.csv", index=False)
-
-    print("Freq_Table.csv is ready")
-
+    writer = pd.ExcelWriter("Freq_Table" + ".xlsx", engine='xlsxwriter')
+    freqTable.to_excel(writer, sheet_name='Mutations Frequencies', index=False)
+    geneFreq.to_excel(writer, sheet_name='gene count')
+    writer.save()
+    print("Freq_Table" + ".csv is ready")
 
 
 if __name__ == '__main__':
